5e7292dfa8d440b5aa612d27a659f859bcc45437
Cai Wei <49383207@qq.com>
2017-05-01 16:12:09 +0800

    Add Trending page time selector and change Android app name

--- src/page/TrendingPage.js
+++ src/page/TrendingPage.js
@@ -23,6 +23,12 @@ import TrendingProjectRow from "../component/TrendingProjectRow";
 
 var popular_def_lans = require('../../res/data/popular_def_lans.json');
 
+const TIME_MAP = new Map([
+    ["Toady", "since=daily"],
+    ["This Week", "since=weekly"],
+    ["This month", "since=monthly"]
+]);
+
 export default class TrendingPage extends Component {
     constructor(props) {
         super(props);
@@ -35,7 +41,8 @@ export default class TrendingPage extends Component {
                 {name: 'JS', isChecked: true}
             ],
             isVisible: false,
-            buttonRect: []
+            buttonRect: [],
+            timeSpan: {key: 'Toady', value: "since=daily"}
         };
         /* popular_def_lans.forEach(item => {
          if (item.checked) this.state.languages.push(item);
@@ -61,17 +68,44 @@ export default class TrendingPage extends Component {
             activeOpacity={0.5}
             onPress={this.showPopover}>
             <View style={{flexDirection:'row',alignItems:'center'}}>
-                <Text style={{color:'#FFF',fontSize:16}}>Trending</Text>
+                <Text style={{color:'#FFF',fontSize:16}}>Trending {this.state.timeSpan.key}</Text>
                 <Image source={require('../../res/images/ic_spinner_triangle.png')}
                        style={{width:12,height:12,marginLeft:5}}/>
             </View>
         </TouchableOpacity>;
     };
 
+    renderRightBtn = () => {
+        return <View style={styles.rightBtn}>
+            <TouchableOpacity
+                activeOpacity={0.7}>
+                <Image source={require('../../res/images/ic_more_vert_white_48pt.png')} style={styles.navBtn}/>
+            </TouchableOpacity>
+        </View>
+    };
+
+    handleTimeSelect = (k, v)=> {
+        this.setState({timeSpan: {key: k, value: v}});
+        this.closePopover();
+    };
+
+    renderTimeMap = ()=> {
+        var views = [];
+        for (let [key,value] of TIME_MAP) {
+            views.push(<TouchableOpacity key={`pop_${value}`} onPress={()=>this.handleTimeSelect(key,value)}>
+                <Text style={{fontSize:18,color:'#FFF',padding:8}}>{key}</Text>
+            </TouchableOpacity>);
+        }
+        return <View style={{alignItems:'center'}}>
+            {views}
+        </View>;
+    };
+
     render() {
         return <View style={styles.container}>
             <NavigationBar
-                titleView={this.renderTitleView()}/>
+                titleView={this.renderTitleView()}
+                rightBtn={this.renderRightBtn()}/>
 
             <ScrollableTabView
                 tabBarBackgroundColor="#63B8FF"
@@ -81,7 +115,8 @@ export default class TrendingPage extends Component {
                 {
                     this.state.languages.map((item, i)=> {
                         return item.isChecked
-                            ? <TrendingTab {...this.props} key={`tab${i}`} tabLabel={item.name}/>
+                            ? <TrendingTab {...this.props} key={`tab${i}`} tabLabel={item.name}
+                                                           timeSpan={this.state.timeSpan}/>
                             : null;
                     })
                 }
@@ -90,10 +125,10 @@ export default class TrendingPage extends Component {
             <Popover
                 isVisible={this.state.isVisible}
                 fromRect={this.state.buttonRect}
-                onClose={this.closePopover}>
-                <Text>By Day</Text>
-                <Text>By Month</Text>
-                <Text>By Year</Text>
+                onClose={this.closePopover}
+                contentStyle={{backgroundColor:'#343434',opacity:0.8}}
+                placement="bottom">
+                {this.renderTimeMap()}
             </Popover>
         </View>
     }
@@ -103,6 +138,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white'
+    },
+    navBtn: {
+        width: 24,
+        height: 24
     }
 });
 
@@ -132,9 +171,9 @@ class TrendingTab extends Component {
         return <TrendingProjectRow item={obj} onSelect={()=>this.handleProjectSelect(obj)}/>
     };
 
-    loadData = ()=> {
+    loadData = (time = "since=daily")=> {
         this.setState({isLoading: true});
-        new GitHubTrending().fetchTrending(`https://github.com/trending/${this.props.tabLabel}?since=daily`)
+        new GitHubTrending().fetchTrending(`https://github.com/trending/${this.props.tabLabel}?${time}`)
             .then(value=> {
                 this.setState({
                     dataSource: this.state.dataSource.cloneWithRows(value),
@@ -168,5 +207,11 @@ class TrendingTab extends Component {
 
     componentDidMount() {
         this.loadData();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.timeSpan.key != this.props.timeSpan.key) {
+            this.loadData(nextProps.timeSpan.value);
+        }
     }
 }
